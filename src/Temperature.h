fdbf4030ffbfbe33612ecbc53f6ecd771e293d17
pokolips <pokolips@gmail.com>
2025-10-19 02:45:50 +0300

    Пересбор

--- src/Temperature.h
+++ src/Temperature.h
@@ -1,9 +1,17 @@
 #pragma once
 
-#include <DallasTemperature.h>
-
+#include <OneWire.h>
+//#include <DallasTemperature.h>
+//#define PIN_DS18B20 8
 // Создаем объект OneWire
-OneWire oneWire(4);
+//OneWire oneWire(PIN_DS18B20);
+OneWire ds(8); // Объект OneWire
+
+// Создаем объект DallasTemperature для работы с сенсорами, передавая ему ссылку на объект для работы с 1-Wire.
+//DallasTemperature dallasSensors(&amp;oneWire);
+
+// Специальный объект для хранения адреса устройства
+//DeviceAddress sensorAddress;
 
 class Temperature
 {
@@ -48,6 +56,44 @@ return _voda;
 // }
 
 
+#include <OneWire.h>
 
+OneWire ds(8); // Объект OneWire
+
+int temperature = 0; // Глобальная переменная для хранения значение температуры с датчика DS18B20
+
+long lastUpdateTime = 0; // Переменная для хранения времени последнего считывания с датчика
+const int TEMP_UPDATE_TIME = 1000; // Определяем периодичность проверок
+
+void setup(){
+  Serial.begin(9600);
+}
+
+void loop(){
+  detectTemperature(); // Определяем температуру от датчика DS18b20
+  Serial.println(temperature); // Выводим полученное значение температуры
+  // Т.к. переменная temperature имеет тип int, дробная часть будет просто отбрасываться
+}
+
+int detectTemperature(){
+
+  byte data[2];
+  ds.reset();
+  ds.write(0xCC);
+  ds.write(0x44);
+
+  if (millis() - lastUpdateTime > TEMP_UPDATE_TIME)
+  {
+    lastUpdateTime = millis();
+    ds.reset();
+    ds.write(0xCC);
+    ds.write(0xBE);
+    data[0] = ds.read();
+    data[1] = ds.read();
+
+    // Формируем значение
+    temperature = (data[1] << 8) + data[0]; temperature = temperature >> 4;
+  }
+}
 
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
-#include"Temperature.h"
+//#include"Temperature.h"
+//#include <DallasTemperature.h>
 
 /*
   Rui Santos
@@ -18,9 +19,23 @@
 // ЗАМЕНИТЕ МАС-АДРЕСОМ ПЛАТЫ-ПОЛУЧАТЕЛЯ
 uint8_t broadcastAddress[] = {0xE8, 0x6B, 0xEA, 0xD4, 0x1F, 0x8C};
  //E8:6B:EA:D4:1F:8C
+// Номер пина Arduino с подключенным датчиком
+
+//--------------------------------------------
+#include <OneWire.h>
+
+OneWire ds(8); // Объект OneWire
+
+int temperature = 0; // Глобальная переменная для хранения значение температуры с датчика DS18B20
+
+long lastUpdateTime = 0; // Переменная для хранения времени последнего считывания с датчика
+const int TEMP_UPDATE_TIME = 1000; // Определяем периодичность проверок
+
+
+
  float tmor = 0;
 
-Temperature tmp;
+//Temperature tmp;
 
 // Структура в скетче платы-отправителя
 // должна совпадать с оной для получателя
@@ -66,14 +81,18 @@ void setup() {
     Serial.println("Failed to add peer");
     return;
   }
-  tmp.settemp();
+  //tmp.settemp();
 }
  
 void loop() {
+
+    detectTemperature(); // Определяем температуру от датчика DS18b20
+  Serial.println(temperature); // Выводим полученное значение температуры
+  // Т.к. переменная temperature имеет тип int, дробная часть будет просто
   // Указываем данные, которые будем отправлять
   strcpy(myData.a, "a");
   myData.b = random(1,20);// оставал старое
-  myData.c = tmp.gettemp();
+  myData.c = 10.2;
   myData.d = "Vanna";
   myData.e = false;
  
@@ -87,4 +106,71 @@ void loop() {
     Serial.println("Error sending the data");
   }
   delay(2000);
-}   
+}  
+int detectTemperature(){
+
+  byte data[2];
+  ds.reset();
+  ds.write(0xCC);
+  ds.write(0x44);
+
+  if (millis() - lastUpdateTime > TEMP_UPDATE_TIME)
+  {
+    lastUpdateTime = millis();
+    ds.reset();
+    ds.write(0xCC);
+    ds.write(0xBE);
+    data[0] = ds.read();
+    data[1] = ds.read();
+
+    // Формируем значение
+    temperature = (data[1] << 8) + data[0]; temperature = temperature >> 4;
+  }
+} 
+
+/*
+#include <DallasTemperature.h>
+
+// Номер пина Arduino с подключенным датчиком
+#define PIN_DS18B20 8
+
+// Создаем объект OneWire
+OneWire oneWire(PIN_DS18B20);
+
+// Создаем объект DallasTemperature для работы с сенсорами, передавая ему ссылку на объект для работы с 1-Wire.
+DallasTemperature dallasSensors(&amp;oneWire);
+
+// Специальный объект для хранения адреса устройства
+DeviceAddress sensorAddress;
+
+void loop(void){
+  // Запрос на измерения датчиком температуры
+
+  Serial.print("Измеряем температуру...");
+  dallasSensors.requestTemperatures(); // Просим ds18b20 собрать данные
+  Serial.println("Выполнено");
+
+  //  Запрос на получение сохраненного значения температуры
+  printTemperature(sensorAddress);
+
+  // Задержка для того, чтобы можно было что-то разобрать на экране
+  delay(1000);
+}
+
+// Вспомогательная функция печати значения температуры для устрйоства
+void printTemperature(DeviceAddress deviceAddress){
+  float tempC = dallasSensors.getTempC(deviceAddress);
+  Serial.print("Temp C: ");
+  Serial.println(tempC);
+}
+
+// Вспомогательная функция для отображения адреса датчика ds18b20
+void printAddress(DeviceAddress deviceAddress){
+  for (uint8_t i = 0; i < 8; i++)
+  {
+    if (deviceAddress[i] < 16) Serial.print("0");
+    Serial.print(deviceAddress[i], HEX);
+  }
+}
+
+*/
